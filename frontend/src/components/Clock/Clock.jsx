c36218f8d6a36d9a3a4fd3b7f77b419cbd543e0a
Niurbelis Gonzalez <niurbelis98@hotmail.com>
2021-09-25 16:20:35 -0400

    create clock component

---
+++ frontend/src/components/Clock/Clock.jsx
@@ -0,0 +1,15 @@
+import { CountdownCircleTimer } from 'react-countdown-circle-timer'
+
+export const Clock = () => (
+  <CountdownCircleTimer
+    isPlaying
+    duration={10}
+    colors={[
+      ['#004777', 0.33],
+      ['#F7B801', 0.33],
+      ['#A30000', 0.33],
+    ]}
+  >
+    {({ remainingTime }) => remainingTime}
+  </CountdownCircleTimer>
+)
